aa090aba8dc9798097a394ba174bdf83329f339f
c2838 <c2838@yahoo.com.tw>
2024-07-02 14:56:50 +0800

    build: git deploy

--- src/api/todos.js
+++ src/api/todos.js
@@ -1,9 +1,26 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3004';
+const baseUrl = 'https://todo-list.alphacamp.io/api';
+
+const axiosInstance = axios.create({ baseURL: baseUrl })
+
+axiosInstance.interceptors.request.use( 
+  (config) => {
+  const token = localStorage.getItem('authToken')
+
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`
+  }
+  return config
+  },
+  (error) => {
+    console.log(error)
+  },
+)
+
 export const getTodos = async () => {
   try {
-    const res = await axios.get(`${baseUrl}/todos`)
-    return res.data
+    const res = await axiosInstance.get(`${baseUrl}/todos`);
+    return res.data.data
 
   } catch(error) {
     console.error('[Get Todos faild]: ', error)
@@ -12,7 +29,7 @@ export const getTodos = async () => {
 export const createTodo = async (payload) => {
   const { title, isDone } = payload
   try {
-    const res = await axios.post(`${baseUrl}/todos`, { title, isDone })
+    const res = await axiosInstance.post(`${baseUrl}/todos`, { title, isDone });
     return res.data
   } catch(error) {
       console.error('[Create Todos faild]: ', error);
@@ -21,9 +38,10 @@ export const createTodo = async (payload) => {
 export const patchTodo = async (payload) => {
   const { id, title, isDone } = payload
   try {
-    const res = await axios.patch(`${baseUrl}/todos/${id}`, {
-      title, isDone
-    })
+    const res = await axiosInstance.patch(`${baseUrl}/todos/${id}`, {
+      title,
+      isDone,
+    });
     return res.data
   } catch(error) {
     console.error('[Patch Todos faild]: ', error);
@@ -31,7 +49,7 @@ export const patchTodo = async (payload) => {
 };
 export const deleteTodo = async (id) => {
   try {
-    const res = await axios.delete(`${baseUrl}/todos/${id}`)
+    const res = await axiosInstance.delete(`${baseUrl}/todos/${id}`);
     return res.data
   } catch(error) {
     console.error('[Delete Todos faild]: ', error);
